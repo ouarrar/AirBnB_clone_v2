613cff7feff592c4ac4e6a49872545f1ee076983
root <ouarrar.abdelouahed@gmail.com>
2024-02-02 00:03:52 +0000

    v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["34.232.76.69", "35.168.1.67"]
+env.hosts = ["35.175.126.60", "34.232.72.18"]
 
 
 def do_pack():
